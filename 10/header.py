4dcbe1743fcb194eee78acb8be86f9d7c8964284
Artem <63151457+pebble690@users.noreply.github.com>
2026-02-15 13:39:12 +0300

    10

--- 10/header.py
+++ 10/header.py
@@ -1,58 +1,39 @@
 import sys
+import numpy as np
 
 class solution:
-    def solve(self) -> result:
-        input_data = sys.stdin.read().split()
-        if not input_data:
+    def solve(self):
+        data = sys.stdin.read().split()
+        if not data:
             return
 
-        n = int(input_data[0])
-        m = int(input_data[1])
-        a = input_data[2]
-        b = input_data[3]
+        n = int(data[0])
+        m = int(data[1])
+        s_a = data[2]
+        s_b = data[3]
 
-        block_size = 21
-        pad = 20
+        a = np.frombuffer(s_a.encode(), dtype=np.int8) - 48
+        b = np.frombuffer(s_b.encode(), dtype=np.int8) - 48
 
-        trans_table = str.maketrans({
-            '1': '0' * pad + '1',
-            '0': '0' * block_size
-        })
+        size = 1
+        while size < n + m:
+            size *= 2
 
-        val_a = int(a[::-1].translate(trans_table), 2)
-        val_b = int(b.translate(trans_table), 2)
-
-        prod = val_a * val_b
+        fa = np.fft.rfft(a, size)
+        fb = np.fft.rfft(b[::-1], size)
         
-        res_str = bin(prod)[2:]
-        len_res = len(res_str)
+        cross_corr = np.fft.irfft(fa * fb, size)
+        matches = np.rint(cross_corr[m-1:n]).astype(np.int32)
 
-        prefix_ones = [0] * (n + 1)
-        current_ones = 0
-        for i in range(n):
-            if a[i] == '1':
-                current_ones += 1
-            prefix_ones[i+1] = current_ones
+        pref = np.zeros(n + 1, dtype=np.int32)
+        np.cumsum(a, out=pref[1:])
+        
+        sum_a_windows = pref[m:n+1] - pref[0:n-m+1]
+        sum_b = np.sum(b)
 
-        ones_b = b.count('1')
-        min_dist = m + 1
-
-        for k in range(n - m + 1):
-            shift = (k + m - 1) * block_size
-            end_idx = len_res - shift
-            start_idx = end_idx - pad
-            
-            matches = 0
-            if end_idx > 0:
-                actual_start = 0 if start_idx < 0 else start_idx
-                if actual_start < end_idx:
-                    matches = int(res_str[actual_start:end_idx], 2)
-
-            dist = (prefix_ones[k + m] - prefix_ones[k]) + ones_b - 2 * matches
-            if dist < min_dist:
-                min_dist = dist
-
-        print(min_dist)
+        distances = sum_a_windows + sum_b - 2 * matches
+        
+        print(np.min(distances))
 
 if __name__ == '__main__':
     solution().solve()
