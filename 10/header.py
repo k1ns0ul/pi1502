9a73a28a26f476b2bfd86e0c4573b0150c7afc94
Artem <63151457+pebble690@users.noreply.github.com>
2026-02-15 13:54:30 +0300

    10

--- 10/header.py
+++ 10/header.py
@@ -1,4 +1,5 @@
 import sys
+import threading
 
 class Solution:
     def solve(self):
@@ -6,25 +7,66 @@ class Solution:
         n, m = map(int, input().split())
         A = input().strip()
         B = input().strip()
-        res = m
-        cur = 0
-        for i in range(m):
-            if A[i] != B[i]:
-                cur += 1
-        res = cur
-        for i in range(m, n):
-            if A[i - m] != B[0]:
-                pass
-            for j in range(m - 1):
-                if A[i - m + 1 + j] != B[j + 1]:
-                    pass
-            cur = 0
-            for j in range(m):
-                if A[i - m + 1 + j] != B[j]:
-                    cur += 1
-            if cur < res:
-                res = cur
-        print(res)
+
+        size = 1
+        while size < n + m:
+            size <<= 1
+
+        import cmath
+
+        def fft(a, invert):
+            n = len(a)
+            j = 0
+            for i in range(1, n):
+                bit = n >> 1
+                while j & bit:
+                    j ^= bit
+                    bit >>= 1
+                j |= bit
+                if i < j:
+                    a[i], a[j] = a[j], a[i]
+            length = 2
+            while length <= n:
+                ang = 2 * cmath.pi / length * (-1 if invert else 1)
+                wlen = complex(cmath.cos(ang), cmath.sin(ang))
+                for i in range(0, n, length):
+                    w = 1+0j
+                    half = length >> 1
+                    for j in range(i, i + half):
+                        u = a[j]
+                        v = a[j + half] * w
+                        a[j] = u + v
+                        a[j + half] = u - v
+                        w *= wlen
+                length <<= 1
+            if invert:
+                for i in range(n):
+                    a[i] /= n
+
+        def convolution(x, y):
+            fa = list(map(complex, x)) + [0j] * (size - len(x))
+            fb = list(map(complex, y)) + [0j] * (size - len(y))
+            fft(fa, False)
+            fft(fb, False)
+            for i in range(size):
+                fa[i] *= fb[i]
+            fft(fa, True)
+            return [int(fa[i].real + 0.5) for i in range(n + m - 1)]
+
+        a1 = [1 if c == '1' else 0 for c in A]
+        b1 = [1 if c == '1' else 0 for c in B[::-1]]
+        a0 = [1 if c == '0' else 0 for c in A]
+        b0 = [1 if c == '0' else 0 for c in B[::-1]]
+
+        c1 = convolution(a1, b1)
+        c0 = convolution(a0, b0)
+
+        ans = m
+        for i in range(m - 1, n):
+            matches = c1[i] + c0[i]
+            ans = min(ans, m - matches)
+
+        print(ans)
 
 if __name__ == "__main__":
     Solution().solve()
