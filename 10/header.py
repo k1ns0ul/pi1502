133972ff232b61c9b7745e2fbfe3360808713746
Artem <63151457+pebble690@users.noreply.github.com>
2026-02-15 14:33:21 +0300

    Update header.py

--- 10/header.py
+++ 10/header.py
@@ -1,39 +1,27 @@
 import sys
-import numpy as np
-from numpy.fft import rfft, irfft
 
 class Solution:
     def solve(self):
-        input_data = sys.stdin.buffer.read().split()
-        n = int(input_data[0])
-        m = int(input_data[1])
-        A = input_data[2].decode()
-        B = input_data[3].decode()
+        data = sys.stdin.buffer.read().split()
+        n = int(data[0])
+        m = int(data[1])
+        A = data[2].decode()
+        B = data[3].decode()
 
-        a = np.array([int(c) for c in A], dtype=np.float64)
-        b = np.array([int(c) for c in B], dtype=np.float64)
+        mask_b = int(B, 2)
+        a_bits = int(A, 2)
+        mask = (1 << m) - 1
+        shift = n - m
 
-        b_rev = b[::-1]
+        best = m
+        for i in range(n - m + 1):
+            xor = ((a_bits >> (shift - i)) & mask) ^ mask_b
+            h = bin(xor).count('1')
+            if h < best:
+                best = h
+                if best == 0:
+                    break
 
-        size = 1
-        while size < n + m:
-            size <<= 1
-
-        fa = rfft(a, size)
-        fb_rev = rfft(b_rev, size)
-        conv_ab = irfft(fa * fb_rev, size)
-
-        a_inv = 1.0 - a
-        fa_inv = rfft(a_inv, size)
-        b_inv = 1.0 - b
-        b_inv_rev = b_inv[::-1]
-        fb_inv_rev = rfft(b_inv_rev, size)
-        conv_ab_inv = irfft(fa_inv * fb_inv_rev, size)
-
-        num_positions = n - m + 1
-        matches = np.round(conv_ab[m-1:m-1+num_positions] + conv_ab_inv[m-1:m-1+num_positions]).astype(np.int64)
-
-        min_hamming = m - int(np.max(matches))
-        print(min_hamming)
+        print(best)
 
 Solution().solve()
