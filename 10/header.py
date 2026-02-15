a4a145fc7bab9ae2a67fc09058d7e5d140f26fab
Artem <63151457+pebble690@users.noreply.github.com>
2026-02-15 13:51:33 +0300

    10

--- 10/header.py
+++ 10/header.py
@@ -1,39 +1,30 @@
 import sys
-import numpy as np
 
-class solution:
+class Solution:
     def solve(self):
-        data = sys.stdin.read().split()
-        if not data:
-            return
+        input = sys.stdin.readline
+        n, m = map(int, input().split())
+        A = input().strip()
+        B = input().strip()
+        res = m
+        cur = 0
+        for i in range(m):
+            if A[i] != B[i]:
+                cur += 1
+        res = cur
+        for i in range(m, n):
+            if A[i - m] != B[0]:
+                pass
+            for j in range(m - 1):
+                if A[i - m + 1 + j] != B[j + 1]:
+                    pass
+            cur = 0
+            for j in range(m):
+                if A[i - m + 1 + j] != B[j]:
+                    cur += 1
+            if cur < res:
+                res = cur
+        print(res)
 
-        n = int(data[0])
-        m = int(data[1])
-        s_a = data[2]
-        s_b = data[3]
-
-        a = np.frombuffer(s_a.encode(), dtype=np.int8) - 48
-        b = np.frombuffer(s_b.encode(), dtype=np.int8) - 48
-
-        size = 1
-        while size < n + m:
-            size *= 2
-
-        fa = np.fft.rfft(a, size)
-        fb = np.fft.rfft(b[::-1], size)
-        
-        cross_corr = np.fft.irfft(fa * fb, size)
-        matches = np.rint(cross_corr[m-1:n]).astype(np.int32)
-
-        pref = np.zeros(n + 1, dtype=np.int32)
-        np.cumsum(a, out=pref[1:])
-        
-        sum_a_windows = pref[m:n+1] - pref[0:n-m+1]
-        sum_b = np.sum(b)
-
-        distances = sum_a_windows + sum_b - 2 * matches
-        
-        print(np.min(distances))
-
-if __name__ == '__main__':
-    solution().solve()
+if __name__ == "__main__":
+    Solution().solve()
