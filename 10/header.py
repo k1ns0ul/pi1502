b531b5efea362a65f9f1b7caf7705ecfbe56c893
Artem <63151457+pebble690@users.noreply.github.com>
2026-02-15 12:53:00 +0300

    2 & 10 new new

--- 10/header.py
+++ 10/header.py
@@ -1,68 +1,63 @@
 import sys
 
+try:
+    sys.set_int_max_str_digits(0)
+except AttributeError:
+    pass
+
 class solution:
     def solve(self):
+
         input_data = sys.stdin.read().split()
+        
         if not input_data:
             return
-        
-        iterator = iter(input_data)
-        try:
-            n = int(next(iterator))
-            m = int(next(iterator))
-            a_str = next(iterator)
-            b_str = next(iterator)
-        except StopIteration:
-            return
 
-        ones_b = b_str.count('1')
-        
-        p = [0] * (n + 1)
-        curr = 0
-        for i in range(n):
-            if a_str[i] == '1':
-                curr += 1
-            p[i+1] = curr
+        n = int(input_data[0])
+        m = int(input_data[1])
+        a = input_data[2]
+        b = input_data[3]
 
-        ba_a = bytearray(n * 3)
-        for i in range(n):
-            if a_str[i] == '1':
-                ba_a[i * 3] = 1
-        
-        ba_b = bytearray(m * 3)
-        for i in range(m):
-            if b_str[i] == '1':
-                idx = (m - 1 - i) * 3
-                ba_b[idx] = 1
-        
-        num_a = int.from_bytes(ba_a, 'little')
-        num_b = int.from_bytes(ba_b, 'little')
-        
-        prod = num_a * num_b
-        
-        total_bytes = (prod.bit_length() + 7) // 8
-        res_ba = prod.to_bytes(total_bytes, 'little')
-        
-        limit = len(res_ba)
-        min_dist = m + 1
-        
-        for i in range(n - m + 1):
-            block_idx = i + m - 1
-            offset = block_idx * 3
+        prefix_a = [0] * (n + 1)
+        cnt = 0
+        for i, c in enumerate(a):
+            if c == '1':
+                cnt += 1
+            prefix_a[i + 1] = cnt
             
-            intersect = 0
-            if offset + 3 <= limit:
-                intersect = res_ba[offset] | (res_ba[offset+1] << 8) | (res_ba[offset+2] << 16)
-            elif offset < limit:
-                intersect = int.from_bytes(res_ba[offset:], 'little')
-            
-            ones_sub_a = p[i + m] - p[i]
-            dist = ones_sub_a + ones_b - 2 * intersect
+        ones_b = b.count('1')
+
+        lookup = {'0': '000000', '1': '000001'}
+        
+        hex_a = "".join([lookup[c] for c in reversed(a)])
+        num_a = int(hex_a, 16)
+        
+        hex_b = "".join([lookup[c] for c in b])
+        num_b = int(hex_b, 16)
+        
+        res = num_a * num_b
+        
+        res_bytes = res.to_bytes((res.bit_length() + 7) // 8, 'little')
+        len_bytes = len(res_bytes)
+        
+        min_dist = m
+        
+        for k in range(n - m + 1):
+
+            byte_idx = (m - 1 + k) * 3
+
+            if byte_idx + 2 < len_bytes:
+                overlap = res_bytes[byte_idx] | (res_bytes[byte_idx+1] << 8) | (res_bytes[byte_idx+2] << 16)
+            else:
+                overlap = 0
+
+            curr_ones_a = prefix_a[k + m] - prefix_a[k]
+            dist = curr_ones_a + ones_b - 2 * overlap
             
             if dist < min_dist:
                 min_dist = dist
-        
+                
         print(min_dist)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     solution().solve()
